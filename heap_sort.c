47ed792e6aa50fac64ac0c2f7f9cdfb65864b592
NIKHIL YADAV <53780673+yadav-nik-hil@users.noreply.github.com>
2020-02-10 12:35:48 +0530

    Add files via upload

--- heap_sort.c
+++ heap_sort.c
@@ -1,30 +1,27 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int N;
 
 void swap(int*,int*);
-void heapSort(int[]);
-void buildMaxheap(int[]);
-void maxHeapify(int[],int);
+void heapSort(int[],int);
+void buildMaxheap(int[],int);
+void maxHeapify(int[],int,int);
 
 
 int main()
 {
-    int i;
+    int i,N;
     do{
         printf("\nEnter the number of elements to be sorted : \t");
         scanf(" %d",&N);
     }while(N<=0);
-    const int n = N;
     int a[N];
 
     printf("\nEnter the elements : \n");
     for(i=0;i<N;i++)
         scanf(" %d",&a[i]);
 
-    heapSort(a);
-    N = n;
+    heapSort(a,N);
     printf("\nSorted array is : \n");
     for(i=0;i<N;i++)
         printf("%d ",a[i]);
@@ -34,14 +31,14 @@ int main()
     return 0;
 }
 
-void buildMaxheap(int a[])
+void buildMaxheap(int a[],int N)
 {
     int i;
     for(i=N/2;i>=0;i--)
-        maxHeapify(a,i);
+        maxHeapify(a,i,N);
 }
 
-void maxHeapify(int a[],int i)
+void maxHeapify(int a[],int i,int N)
 {
     int l = (2*i)+1 ,r = (2*i)+2;
     int max = (l<N && a[l]>a[i]) ? l : i;
@@ -50,17 +47,17 @@ void maxHeapify(int a[],int i)
     if(max!=i)
     {
         swap(&a[i],&a[max]);
-        maxHeapify(a,max);
+        maxHeapify(a,max,N);
     }
 }
 
-void heapSort(int a[])
+void heapSort(int a[],int N)
 {
-    buildMaxheap(a);
+    buildMaxheap(a,N);
     while(N)
     {
         swap(&a[0],&a[--N]);
-        maxHeapify(a,0);
+        maxHeapify(a,0,N);
     }
 }
 
